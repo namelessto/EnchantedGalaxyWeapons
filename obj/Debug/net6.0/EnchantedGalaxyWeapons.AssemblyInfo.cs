117e88bd397f9b7501d8b962ac7e5bed5fac0f5b
Shai <shaitnto@gmail.com>
2024-04-30 09:54:33 +0300

    add Chinese translation file

--- obj/Debug/net6.0/EnchantedGalaxyWeapons.AssemblyInfo.cs
+++ obj/Debug/net6.0/EnchantedGalaxyWeapons.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnchantedGalaxyWeapons")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4202224bd620395245bc3c7c1197d006bc20eb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d502dfdc41946fc612953845509f8401b794bba1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnchantedGalaxyWeapons")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnchantedGalaxyWeapons")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
